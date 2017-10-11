39a9af9e8eb332aeb218cebfcc77480d3fcd21e5
Unknown <garciadiego769@gmail.com>
2017-10-11 19:24:04 +0200

    Ejercicio 7 resuelto

---
+++ 07_valor_absoluto/Ejercicio 7/src/garcia/diego/Main.java
@@ -0,0 +1,25 @@
+package garcia.diego;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+	// write your code here
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Introduce un número");
+        int numero=Integer.parseInt(br.readLine());
+
+        if (numero!=0){
+            if(numero <0){
+                int operacion=numero*(-1);
+                System.out.println("Valor absouluto: " + operacion);
+            }
+            else{
+                System.out.println("Valor absoluto: " +numero);
+            }
+        }
+    }
+}
